c45e3c3734b2616a9fe367ba95930a6792c4e10e
RFNshare <rfnshare@gmail.com>
2020-07-14 13:12:08 +0600

    Organize Dishes Component with shared folder

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Navbar, NavbarBrand} from "reactstrap";
 import './App.css';
 import Menu from "./components/MenuComponent";
-import SampleMenu from "./components/PracticeComponent";
+// import SampleMenu from "./components/PracticeComponent";
+import {DISHES} from "./shared/dishes";
 
-function App() {
-  return (
-    <div>
-      <Navbar dark color="primary">
-        <div className="container">
-          <NavbarBrand href="/">Crazy Chef</NavbarBrand>
-        </div>
-      </Navbar>
-        <Menu />
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state =
+            {
+                dishes: DISHES
+            }
+    }
+
+    render() {
+        return (
+            <div>
+                <Navbar dark color="primary">
+                    <div className="container">
+                        <NavbarBrand href="/">Crazy Chef</NavbarBrand>
+                    </div>
+                </Navbar>
+                <Menu dishes={this.state.dishes}/>
+            </div>
+        );
+    }
 }
 
 export default App;
